9edd6fcdacf9131756050f27b5f8a66aad5b36c0
Tran <ready0110@gmail.com>
2022-09-25 21:44:29 +0700

    update code

--- src/BTGioHangRedux/DanhSachRedux.jsx
+++ src/BTGioHangRedux/DanhSachRedux.jsx
@@ -3,14 +3,14 @@ import SanPhamRedux from './SanPhamRedux'
 
 export default class DanhSachRedux extends Component {
 
-    renderPhoneList = () => {
-        return this.props.phoneList.map((phone) => { 
-            return <div className="col-4" key={phone.maSP}>
-                <SanPhamRedux phone={phone}/>
-            </div>
-            
-         })
-    }
+  renderPhoneList = () => {
+    return this.props.phoneList.map((phone) => {
+      return <div className="col-4" key={phone.maSP}>
+        <SanPhamRedux phone={phone} />
+      </div>
+
+    })
+  }
   render() {
     return (
       <div className='row py-5'>
--- src/BTGioHangRedux/SanPhamRedux.jsx
+++ src/BTGioHangRedux/SanPhamRedux.jsx
@@ -3,33 +3,25 @@ import { connect, Connect } from "react-redux";
 
 class SanPhamRedux extends Component {
   render() {
-    console.log(this.props)
-    let {phone} = this.props;
+    //console.log(this.props)
+    let { phone } = this.props;
     return (
       <div className="card">
         <img className="card-img-top" src={phone.hinhAnh} alt="" />
         <div className="card-body">
           <h4 className="card-title">{phone.tenSP}</h4>
           <p className="card-text">{phone.giaBan}</p>
-          <button
-            onClick={() => {
+          <button onClick={() => {
               // Đẩy dữ liệu lên store
               // C1: sử dụng hàmg dispatch(): gửi dữ liêu  từ compo lên store
               // để đẩy data lên store thì phải tạo action object
               const action = {
-                type:"THEM_GIO_HANG", // bắt buộc phải có
-                sanPham: phone
-
-              }
-
+                type: "THEM_GIO_HANG", // bắt buộc phải có
+                sanPham: phone 
+              } 
               this.props.dispatch(action)
             }}
-            data-toggle="modal"
-            data-target="#modelId"
-            className="btn btn-danger"
-          >
-            Thêm giỏ hàng
-          </button>
+            data-toggle="modal" data-target="#modelId" className="btn btn-danger" > Thêm giỏ hàng </button>
         </div>
       </div>
     );
